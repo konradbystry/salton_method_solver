12cbfd7a5a98a020c7a52ec3a546a444e211c38f
Konrad Bystrzykowski <konor2000@gmail.com>
2021-02-26 22:30:59 +0100

    created obj array

--- src/main.java
+++ src/main.java
@@ -1,17 +1,16 @@
-import jdk.internal.jshell.tool.resources.l10n;
+import Rocchia.*;
 
 public class main {
     public static void main(String args[]) {
-        Rocchia rocchia = new Rocchia();
+        Corelation rocchia = new Corelation();
 
-        char first[] = { 'a', 'b', 'c', 'd', 'e' };
-        char second[] = { 'a', 'b', 'c', 'd' };
-        int numerator = rocchia.setNumerator(first, second);
-        int denominator = rocchia.setDenominator(first, second);
-
-        System.out.println(rocchia.corelation(numerator, denominator));
-        System.out.println(numerator);
-        System.out.println(denominator);
-
+        Objects obj = new Objects("abcde", "x1");
+        Objects objs[] = { new Objects("abcde", "x1"), new Objects("abcd", "x2") };
+        String test[] = { "abcde", "abcd" };
+        rocchia.allObjCorelation(test, "abcd");
+        rocchia.sort(rocchia.corelations);
+        for (double x : rocchia.corelations) {
+            System.out.println(x);
+        }
     }
 }
