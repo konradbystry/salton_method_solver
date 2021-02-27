ea13187b56636641fa4cd118ecb0ae8d1e8e2065
Konrad Bystrzykowski <konor2000@gmail.com>
2021-02-27 22:37:30 +0100

    created setRank method and setted up Collections.sort to work proply

--- src/Rocchia/Corelation.java
+++ src/Rocchia/Corelation.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 import java.util.Collection;
+import java.util.Comparator;
 
 public class Corelation {
 
-    public String objects[];
-    public List<Double> corelations = new ArrayList<Double>();
+    public Objects objects[];
+    public List<Objects> corelations = new ArrayList<Objects>();
+    public List<Objects> corelationsSorted = new ArrayList<Objects>();
 
     public int setNumerator(char firstObj[], char secObj[]) {
 
@@ -39,17 +41,29 @@ public class Corelation {
         return numerator / denominator;
     }
 
-    public void allObjCorelation(String objects[], String groupCenter) {
+    public void allObjCorelation(Objects objects[], String groupCenter) {
         char cent[] = groupCenter.toCharArray();
 
         for (int i = 0; i < objects.length; i++) {
-            char obj[] = objects[i].toCharArray();
-            corelations.add(corelation(setNumerator(obj, cent), setDenominator(obj, cent)));
+            Objects o = new Objects(objects[i].getValue(), objects[i].getName());
+            char obj[] = objects[i].getValue().toCharArray();
+            o.setCor(corelation(setNumerator(obj, cent), setDenominator(obj, cent)));
+            corelations.add(o);
         }
     }
 
-    public void sort(List<Double> corelations) {
+    public void setRanks(List<Objects> corelations) {
+
+        for (int i = 0; i < corelations.size(); i++) {
+            for (int j = 0; j < corelations.size(); j++) {
+                if (corelations.get(i).getCor() < corelations.get(j).getCor()) {
+                    corelations.get(i).incRank();
+                }
+            }
+        }
+    }
+
+    public void sort(List<Objects> corelations) {
         Collections.sort(corelations);
-        Collections.reverse(corelations);
     }
 }
