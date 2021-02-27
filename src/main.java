c4ccc41c4880507b6e582c12ad40e556e8639f73
Konrad Bystrzykowski <konor2000@gmail.com>
2021-02-27 22:32:45 +0100

    implemented sorting by ranks

--- src/main.java
+++ src/main.java
@@ -4,13 +4,13 @@ public class main {
     public static void main(String args[]) {
         Corelation rocchia = new Corelation();
 
-        Objects obj = new Objects("abcde", "x1");
-        Objects objs[] = { new Objects("abcde", "x1"), new Objects("abcd", "x2") };
-        String test[] = { "abcde", "abcd" };
-        rocchia.allObjCorelation(test, "abcd");
+        Objects objs[] = { new Objects("abcde", "x1"), new Objects("abcd", "x2"), new Objects("abcdef", "x3") };
+        rocchia.allObjCorelation(objs, "abcd");
+        rocchia.setRanks(rocchia.corelations);
         rocchia.sort(rocchia.corelations);
-        for (double x : rocchia.corelations) {
-            System.out.println(x);
+
+        for (Objects cor : rocchia.corelations) {
+            System.out.println(cor.getName() + " " + cor.getCor() + " " + cor.getRank());
         }
     }
 }
