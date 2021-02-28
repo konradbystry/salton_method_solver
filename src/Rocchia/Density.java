f9b76e66c35a1486dacc5c13f771eed82a5d5010
Konrad Bystrzykowski <konor2000@gmail.com>
2021-02-28 20:52:49 +0100

    fixed setPmin function

--- src/Rocchia/Density.java
+++ src/Rocchia/Density.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class Density extends Corelation {
     int n1, n2, n1p, n2p;
     double p1, p2, p1p, p2p;
+    public List<Objects> m1 = new ArrayList<Objects>();
+    public List<Objects> m2 = new ArrayList<Objects>();
+    private Iterator i = m2.iterator();
 
-    private List<Double> corelations = new ArrayList<Double>();
+    // private List<Double> corelations = new ArrayList<Double>();
 
-    Density(List<Double> corelations) {
-        this.corelations = corelations;
-    }
-
+    /*
+     * Density(List<Double> corelations) { this.corelations = corelations; }
+     */
     public void setP1(double p1) {
         this.p1 = p1;
     }
@@ -79,20 +82,53 @@ public class Density extends Corelation {
         return n2p;
     }
 
-    public boolean densityTest(List<Double> corelations) {
+    public boolean densityTest(List<Objects> corelations) {
         int counterN1 = 0;
         int counterN2 = 0;
-        for (double cor : corelations) {
-            if (cor >= getP1()) {
+        for (Objects cor : corelations) {
+            if (cor.getCor() >= getP1()) {
                 counterN1++;
             }
-            if (cor >= getP2()) {
+            if (cor.getCor() >= getP2()) {
                 counterN2++;
             }
         }
         if (counterN1 >= getN1() && counterN2 >= getN2()) {
+            System.out.println("succes");
             return true;
         } else
-            return false;
+            System.out.println("failed");
+        return false;
+    }
+
+    public void setGroups(List<Objects> obj) {
+
+        for (Objects o : obj) {
+            if (o.getCor() >= getP2()) {
+                m2.add(o);
+            }
+            if (o.getCor() >= getP1()) {
+                m1.add(o);
+            }
+        }
+    }
+
+    public double setPmin(List<Objects> obj) {
+        double pmin = m2.get(m2.size() - 1).getCor();
+        double currentMin = 0;
+        double result = 0;
+
+        for (Objects o : obj) {
+
+            currentMin = pmin - o.getCor();
+
+            if (currentMin > result && o.getCor() <= m2.get(m2.size() - 1).getCor()) {
+                result = o.getCor();
+
+            }
+            pmin = o.getCor();
+        }
+
+        return result;
     }
 }
